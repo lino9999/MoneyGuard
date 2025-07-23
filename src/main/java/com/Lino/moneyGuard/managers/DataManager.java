7093fa975aa1334d35e492f8089a1f0e5b496e67
Lino <lino9999.123@gmail.com>
2025-07-23 03:17:05 +0200

    c

--- src/main/java/com/Lino/moneyGuard/managers/DataManager.java
+++ src/main/java/com/Lino/moneyGuard/managers/DataManager.java
@@ -2,11 +2,7 @@ package com.Lino.moneyGuard.managers;
 
 import com.Lino.moneyGuard.MoneyGuard;
 import com.Lino.moneyGuard.data.PlayerData;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,8 +11,6 @@ public class DataManager {
 
     private final MoneyGuard plugin;
     private final Map<UUID, PlayerData> playerDataMap;
-    private File dataFile;
-    private FileConfiguration dataConfig;
 
     public DataManager(MoneyGuard plugin) {
         this.plugin = plugin;
@@ -24,57 +18,11 @@ public class DataManager {
     }
 
     public void loadData() {
-        dataFile = new File(plugin.getDataFolder(), "playerdata.yml");
 
-        if (!dataFile.exists()) {
-            try {
-                dataFile.createNewFile();
-            } catch (IOException e) {
-                plugin.getLogger().severe("Could not create playerdata.yml!");
-            }
-        }
-
-        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
-
-        if (dataConfig.contains("players")) {
-            for (String uuidString : dataConfig.getConfigurationSection("players").getKeys(false)) {
-                UUID uuid = UUID.fromString(uuidString);
-                PlayerData data = new PlayerData(uuid);
-
-                String path = "players." + uuidString + ".";
-
-                if (dataConfig.getBoolean(path + "banned", false)) {
-                    long banExpiry = dataConfig.getLong(path + "ban-expiry", 0);
-                    if (banExpiry > System.currentTimeMillis()) {
-                        data.ban(banExpiry - System.currentTimeMillis());
-                    }
-                }
-
-                playerDataMap.put(uuid, data);
-            }
-        }
     }
 
     public void saveAllData() {
-        for (Map.Entry<UUID, PlayerData> entry : playerDataMap.entrySet()) {
-            savePlayerData(entry.getKey(), entry.getValue());
-        }
 
-        try {
-            dataConfig.save(dataFile);
-        } catch (IOException e) {
-            plugin.getLogger().severe("Could not save playerdata.yml!");
-        }
-    }
-
-    private void savePlayerData(UUID uuid, PlayerData data) {
-        String path = "players." + uuid.toString() + ".";
-
-        dataConfig.set(path + "banned", data.isBanned());
-
-        if (data.isBanned()) {
-            dataConfig.set(path + "ban-expiry", data.getBanExpiry());
-        }
     }
 
     public PlayerData getPlayerData(UUID uuid) {
@@ -83,7 +31,6 @@ public class DataManager {
 
     public void removePlayerData(UUID uuid) {
         playerDataMap.remove(uuid);
-        dataConfig.set("players." + uuid.toString(), null);
     }
 
     public void clearAll5MinuteData() {
